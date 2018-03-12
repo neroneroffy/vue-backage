7f872c496c4c7d71926722575f37ebf2e6a700a7
fzshigs <1440914641@qq.com>
2018-03-12 11:45:39 +0800

    盘点

--- vue_font_end/src/containers/repertory/stock-taking/stock-taking.vue
+++ vue_font_end/src/containers/repertory/stock-taking/stock-taking.vue
@@ -4,12 +4,13 @@
       <Col span="4">
       <DatePicker type="daterange" v-model="time" :value="time" on-change="time"></DatePicker>
       </Col>
+
       <Col sapn="18">
       <Form ref="formInline" :model="searchContent" inline>
         <FormItem prop="id">
-          <Select v-model="searchContent.id" style="width:200px" placeholder="请选择仓库">
+          <Select v-model="searchContent.id" :value="searchContent.id" style="width:200px" placeholder="请选择仓库">
             <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
-          </Select
+          </Select>
         </FormItem>
         <FormItem prop="account">
           <Input type="text" v-model="searchContent.account" placeholder="请输入搜索账户"/>
@@ -21,15 +22,19 @@
           <Button type="primary" icon="ios-search" @click="handleSubmit('formInline')">搜索</Button>
         </FormItem>
         <FormItem>
-          <Button type="primary" icon="ios-archive" @click="">导出盘点单</Button>
+          <Button type="primary" icon="ios-archive" @click="educe()">导出盘点单</Button>
         </FormItem>
         <FormItem>
-          <Button type="primary" icon="ios-search" @click="">生成盘点单</Button>
+          <Button type="primary" icon="ios-search" @click="make()">生成盘点单</Button>
         </FormItem>
       </Form>
       </Col>
     </Row>
-
+    <Tabs v-model="status" :value="status">
+      <TabPane label="商品" name="commodity" >标签一的内容</TabPane>
+      <TabPane label="赠品" name="present" >标签二的内容</TabPane>
+      <TabPane label="物料" name="materiel" >标签三的内容</TabPane>
+    </Tabs>
   </div>
 </template>
 
@@ -48,7 +53,12 @@
           account:"",
           phone:""
         },
+        status:"commodity",
         cityList:[
+          {
+            value: '',
+            label: '请选择仓库'
+          },
           {
             value: 'New York',
             label: 'New York'
@@ -68,10 +78,18 @@
 
     },
     methods:{
-      handleSubmit(params){
-        console.log(params)
+      //提交搜索
+      handleSubmit(){
+        console.log(this.searchContent)
+      },
+      //导出盘点单
+      educe(){
+        console.log('educe')
+      },
+      //生成盘点单
+      make(){
+        console.log('make')
       }
-
     }
 
 
